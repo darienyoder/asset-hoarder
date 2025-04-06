2ed30a4b3a7f3b82fc1bb622f322726bb68d4c4a
SageAndromeda <sagehardiman@gmail.com>
2025-04-05 23:47:05 -0400

    Yay password validation

--- FrontEnd/signupLogin.js
+++ FrontEnd/signupLogin.js
@@ -64,3 +64,42 @@ function closeModal(modal){
     modal.classList.remove('active')
     overlay.classList.remove('active')
 }
+
+var passConfirm = function() {
+    if ((document.getElementById("new-pass").value ==
+        document.getElementById("new-pass-rep").value) && 
+        (document.getElementById("new-pass-rep").value != "") &&
+        (document.getElementById("new-pass-rep").value != null)) {
+            document.getElementById("pass-match-output").style.color = "#00811f";
+            document.getElementById("pass-match-output").innerHTML = "&#x2714; Passwords match!"
+    } else {
+        document.getElementById("pass-match-output").style.color = "#943824";
+        document.getElementById("pass-match-output").innerHTML = "&#x2716; Passwords do NOT match!"
+    }
+}
+
+var properPass = function() {
+    if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$/.test
+        (document.getElementById("new-pass").value)){
+        document.getElementById("passwordNotes").style.color = "#00811f";
+        document.getElementById("passwordNotes").innerHTML = "Valid password!"
+    }
+    else {
+        document.getElementById("passwordNotes").style.color = "#943824";
+        document.getElementById("passwordNotes").innerHTML = 
+        "Your password must be between 8 and 32 characters, contain a lowercase letter, uppercase letter, and a special character (@, $, !, %, *, ?, or &)."
+    }
+}
+
+var properUser = function() {
+    if (/^[a-zA-Z0-9_]{3,16}$/.test
+        (document.getElementById("new-user").value)){
+        document.getElementById("usernameNotes").style.color = "#00811f";
+        document.getElementById("usernameNotes").innerHTML = "Valid username!"
+    }
+    else {
+        document.getElementById("usernameNotes").style.color = "#943824";
+        document.getElementById("usernameNotes").innerHTML = 
+        "Your username must be at least 3 chararacters and only include letters, numbers, and underscores."
+    }
+}
