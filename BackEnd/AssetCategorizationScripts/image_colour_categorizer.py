08444ad85a8d372ac6060d418802832bfecdc8d5
Ozpyn <gmingle@kent.edu>
2025-04-16 13:09:38 -0400

    Its not as simple as black and white.

--- BackEnd/AssetCategorizationScripts/image_colour_categorizer.py
+++ BackEnd/AssetCategorizationScripts/image_colour_categorizer.py
@@ -20,9 +20,7 @@ PREDEFINED_COLORS = {
     "Yellow": "#FFFF00",
     "Green": "#008000",
     "Blue": "#0000FF",
-    "Violet": "#7F00FF",
-    "White": "#FFFFFF",
-    "Black": "#000000"
+    "Violet": "#7F00FF"
 }
 
 ### --- DATABASE HELPERS --- ###
